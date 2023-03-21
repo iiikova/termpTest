47ba6aefcc790c98a15e8c967ca0783bafe74593
Ihor Koval <ikova.ua@gmail.com>
2023-03-21 10:40:50 +0200

    testewfwfwe fwef

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public abstract class Main extends Dog  {
     public static void main(String[] args) {
-       for (int i = 0; i < 10; i++){
-           if (i%2 == 0){
-               continue;
-           }
-           System.out.println(i);
-       }
+     int[][] num = {
+             {1,2,3,4},
+                   {4,5,6,7},
+             {5,6,8,9}};
+     for (int i = 0; i< num.length ;i++){
+         for (int j = 0; j < num[i].length ; j++) {
+             System.out.print(num[i][j] + " ");
+         }
+         System.out.println();
+     }
 
 }}
